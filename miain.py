2a6d0a012264b08f6d4b06af654ce2d169274377
Percent <107308940+OCC-Percent@users.noreply.github.com>
2022-06-12 11:53:22 +0800

    File Update
    1. Add the title.

--- miain.py
+++ miain.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import tkinter as tk
 
 window = tk.Tk()
@@ -6,6 +7,13 @@ window.geometry("1280x720+325+150")
 window.config(bg="#323232")
 
 profile_img = tk.PhotoImage(file="D:\Code\occisor_app\profile_logo.png")
+
+title = tk.Label()
+title.config(bg="#323232")
+title.config(fg="#FFFFFF")
+title.config(text="The Occisor Application")
+title.config()
+title.pack()
 
 profile_btn = tk.Button()
 profile_btn.config(bg="#323232")
