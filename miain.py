1ff271009efff116d93ff93e30af6411585c3550
Percent <107308940+OCC-Percent@users.noreply.github.com>
2022-06-12 11:51:38 +0800

    File Update
    1. Add the title to the main interface.
    2. Add the profile button, but there's no function yet.

--- miain.py
+++ miain.py
@@ -1,1 +1,16 @@
+import tkinter as tk
 
+window = tk.Tk()
+window.title('Random Coordinate Generator')
+window.geometry("1280x720+325+150")
+window.config(bg="#323232")
+
+profile_img = tk.PhotoImage(file="D:\Code\occisor_app\profile_logo.png")
+
+profile_btn = tk.Button()
+profile_btn.config(bg="#323232")
+profile_btn.config(fg="#FFFFFF")
+profile_btn.config(image=profile_img)
+profile_btn.pack()
+
+window.mainloop()
